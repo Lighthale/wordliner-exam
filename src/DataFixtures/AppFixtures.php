f10c2a634648380dd3afbe7ebe96d3b01e786b00
Bryan Mangulabnan <mangulabnan.bryan@ymail.com>
2026-04-12 16:08:01 +0800

    Oauth2 with grant type (league/oauth2-server-bundle)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('admin@recruitday.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword('password');
+        $manager->persist($user);
 
         $manager->flush();
     }
---
+++ src/EventListener/UserResolveListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventListener;
+
+use League\Bundle\OAuth2ServerBundle\Event\UserResolveEvent;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+final class UserResolveListener
+{
+    public function __construct(
+        private readonly UserProviderInterface       $userProvider,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {}
+
+    public function onUserResolve(UserResolveEvent $event): void
+    {
+        try {
+            $user = $this->userProvider->loadUserByIdentifier($event->getUsername());
+        } catch (AuthenticationException $e) {
+            return;
+        }
+
+        if (!$user instanceof PasswordAuthenticatedUserInterface) {
+            return;
+        }
+
+        if (!$this->passwordHasher->isPasswordValid($user, $event->getPassword())) {
+            return;
+        }
+
+        $event->setUser($user);
+    }
+}
